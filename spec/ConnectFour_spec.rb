96ddcba41f16b160f5f5362974e1028704a5fd27
Razvan Rotundu <raz.rotundu@gmail.com>
2024-10-21 19:47:26 -0400

    Finish contructor test

--- spec/ConnectFour_spec.rb
+++ spec/ConnectFour_spec.rb
@@ -4,16 +4,27 @@ require_relative "../lib/ConnectFour"
 
 describe ConnectFour do
   describe "Constructor" do
+    b1 = ConnectFour.new("b")
+    b2 = ConnectFour.new("2")
+    b3 = ConnectFour.new()
+
+    black = "\u26ab".encode('utf-16')
+    white = "\u26aa".encode('utf-16')
+
     it "Inits starting player correctly" do
       
-      expect(true).to eq(true)
+      expect(b1.current_player).to eq(black)
     end
 
-    it "rejects no arguments" do
+    it "rejects bad arguments" do
       
-      expect(true).to eq(true)
+      expect(b2).to eq(nil)
     end
 
+    it "takes default arguments" do
+      
+      expect(b3.current_player).to eq(white)
+    end
   end
 
   describe "make_move" do
