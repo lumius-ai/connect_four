75f0cb2dd2aaf625b28d9c7b2e9f56b6f1bde583
Razvan Rotundu <raz.rotundu@gmail.com>
2024-10-21 19:32:26 -0400

    Add function skeletons to class

---
+++ lib/ConnectFour.rb
@@ -0,0 +1,26 @@
+# Connect four game
+
+class ConnectFour
+  attr_accessor :board, :current_player
+
+  def initialize(player)
+  end
+
+  def make_move(col)
+
+  end
+
+  def check_win
+
+  end
+
+
+  def to_s
+
+  end
+
+  private
+  def check_row9(row)
+
+  end
+end
