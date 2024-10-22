5a35bf671b11a98062cb80ec57e8d3eb76365c0d
Razvan Rotundu <raz.rotundu@gmail.com>
2024-10-22 11:02:01 -0400

    Complete constructor

--- lib/ConnectFour.rb
+++ lib/ConnectFour.rb
@@ -3,8 +3,25 @@
 class ConnectFour
   attr_accessor :board, :current_player
 
-  def initialize(player)
+  def initialize(args = {})
+    black = "\u26ab".encode('utf-8')
+    white = "\u26aa".encode('utf-8')
+
+    case args[:current_player]
+
+    when 'b'
+      @current_player = black
+    when 'w'
+      @current_player = white
+    when nil
+      @current_player = white
+    else
+      @current_player = nil
+    end
+
+    args[:board].nil? ? @board = Array.new(4){Array.new(4)} : @board = args[:board]
   end
+
 
   def make_move(col)
 
--- spec/ConnectFour_spec.rb
+++ spec/ConnectFour_spec.rb
@@ -3,13 +3,16 @@
 require_relative "../lib/ConnectFour"
 
 describe ConnectFour do
+  black = "\u26ab".encode('utf-8')
+  white = "\u26aa".encode('utf-8')
+
   describe "Constructor" do
-    b1 = ConnectFour.new("b")
-    b2 = ConnectFour.new("2")
+    b1 = ConnectFour.new({:current_player =>'b'})
+    b2 = ConnectFour.new({:current_player => "2"})
     b3 = ConnectFour.new()
 
-    black = "\u26ab".encode('utf-16')
-    white = "\u26aa".encode('utf-16')
+    board = Array.new(4){Array.new(4)}
+    b4 = ConnectFour.new({:board => board})
 
     it "Inits starting player correctly" do
       
@@ -18,53 +21,58 @@ describe ConnectFour do
 
     it "rejects bad arguments" do
       
-      expect(b2).to eq(nil)
+      expect(b2.current_player).to eq(nil)
     end
 
     it "takes default arguments" do
       
       expect(b3.current_player).to eq(white)
     end
-  end
 
-  describe "make_move" do
-    it "places piece at bottom of column" do
-      
-      expect(true).to eq(true)
-    end
+    it "takes custom board" do
 
-    it "properly stacks pieces" do
-      
-      expect(true).to eq(true)
+      expect(b4.board).to eq(board)
     end
   end
 
-  describe "check_win" do
-    it "registers horizontal win" do
+  # describe "make_move" do
+  #   it "places piece at bottom of column" do
       
-      expect(true).to eq(true)
-    end
+  #     expect(true).to eq(true)
+  #   end
 
-    it "registers vertical win" do
+  #   it "properly stacks pieces" do
       
-      expect(true).to eq(true)
-    end
+  #     expect(true).to eq(true)
+  #   end
+  # end
 
-    it "registers diagonal win" do
+  # describe "check_win" do
+  #   it "registers horizontal win" do
       
-      expect(true).to eq(true)
-    end
+  #     expect(true).to eq(true)
+  #   end
 
-    it "registers no win" do
+  #   it "registers vertical win" do
       
-      expect(true).to eq(true)
-    end
-  end
+  #     expect(true).to eq(true)
+  #   end
 
-  describe "to_s" do
-    it "Prints board content correctly" do
+  #   it "registers diagonal win" do
       
-      expect(true).to eq(true)
-    end
-  end
+  #     expect(true).to eq(true)
+  #   end
+
+  #   it "registers no win" do
+      
+  #     expect(true).to eq(true)
+  #   end
+  # end
+
+  # describe "to_s" do
+  #   it "Prints board content correctly" do
+      
+  #     expect(true).to eq(true)
+  #   end
+  # end
 end
